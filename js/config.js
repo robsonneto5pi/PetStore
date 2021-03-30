fa84fa9c62ecb15fac4233adb9fa2160f15f247b
robsonneto5pi <robsonneto5@gmail.com>
2021-03-29 23:06:50 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: '', // e.g. 'sa-east-1_Z40q2zY4P'
+        userPoolClientId: '', // e.g. '59r6l3s5mj7pi248nvn4907ti9'
+        region: '' // e.g. 'sa-east-1'
+    },
+    api: {
+        invokeUrl: 'https://jj28rfigl6.execute-api.sa-east-1.amazonaws.com/prod' // e.g. 'https://ibaibayghc.execute-api.sa-east-1.amazonaws.com/prod'
+    }
+};
