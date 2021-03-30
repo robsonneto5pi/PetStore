d04ecf82ec62e95b19d9818ea8348f5b60d0afa8
robsonneto5pi <robsonneto5@gmail.com>
2021-03-29 23:32:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. 'sa-east-1_Z40q2zY4P'
-        userPoolClientId: '', // e.g. '59r6l3s5mj7pi248nvn4907ti9'
-        region: '' // e.g. 'sa-east-1'
+        userPoolId: 'sa-east-1_Z40q2zY4P', // e.g. 'sa-east-1_Z40q2zY4P'
+        userPoolClientId: '59r6l3s5mj7pi248nvn4907ti9', // e.g. '59r6l3s5mj7pi248nvn4907ti9'
+        region: 'sa-east-1' // e.g. 'sa-east-1'
     },
     api: {
         invokeUrl: 'https://jj28rfigl6.execute-api.sa-east-1.amazonaws.com/prod' // e.g. 'https://ibaibayghc.execute-api.sa-east-1.amazonaws.com/prod'
     }
 };
+ 
